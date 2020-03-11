259fc1bd09fb8714059605ab85e16332ef1d3240
Carlos Jaime Cobaleda Cruz <charlie2634@gmail.com>
2020-03-11 15:32:10 -0500

    Filter changes and FilterSearchResultActivity creation

--- app/src/main/java/com/example/astromedics/views/TherapistFilterActivity.java
+++ app/src/main/java/com/example/astromedics/views/TherapistFilterActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
@@ -13,38 +14,52 @@ import android.widget.EditText;
 
 import com.example.astromedics.R;
 import com.example.astromedics.views.fragments.DatePickerFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
 
 public class TherapistFilterActivity extends AppCompatActivity {
-
-    EditText etPlannedDate;
+    EditText startDate, endDate ;
+    private FloatingActionButton button;
 
      @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_therapist_filter);
 
-         etPlannedDate = (EditText) findViewById(R.id.etPlannedDate);
-         etPlannedDate.setOnClickListener(new View.OnClickListener() {
+         button = findViewById(R.id.confirmFilters);
+         startDate = findViewById(R.id.startDate);
+         endDate = findViewById(R.id.endDate);
+
+         startDate.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
-                 switch (view.getId()) {
-                     case R.id.etPlannedDate:
-                         showDatePickerDialog();
-                         break;
-                 }
+                 showDatePickerDialog(startDate);
+             }
+         });
+         endDate.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 showDatePickerDialog(endDate);
+             }
+         });
+         button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent=new Intent(v.getContext(), TherapistSearchResultActivity.class);
+                 startActivity(intent);
              }
          });
      }
 
-    private void showDatePickerDialog() {
+    private void showDatePickerDialog(final EditText editText) {
         DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
 
                 final String selectedDate = day + " / " + (month+1) + " / " + year;
-                etPlannedDate.setText(selectedDate);
+                editText.setText(selectedDate);
             }
         });
         newFragment.show(getSupportFragmentManager(), "datePicker");
