eb504633502d7827b8f158d4855685d4d7d6fbe6
charlie2634 <charlie2634@gmail.com>
2020-04-06 20:07:13 -0500

    Checkpoint 7

--- app/src/main/java/com/example/astromedics/adapters/MedicalConsultationAdapter.java
+++ app/src/main/java/com/example/astromedics/adapters/MedicalConsultationAdapter.java
@@ -1,9 +1,6 @@
 package com.example.astromedics.adapters;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,13 +12,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.astromedics.R;
+import com.example.astromedics.helpers.ApplicationDateFormat;
+import com.example.astromedics.helpers.DownloadImageTask;
 import com.example.astromedics.model.Appointment;
 import com.example.astromedics.model.MedicalConsultation;
 import com.example.astromedics.model.Therapist;
 import com.example.astromedics.repository.Repository;
 
-import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 public class MedicalConsultationAdapter extends ArrayAdapter<MedicalConsultation> {
     private int resourceLayout;
@@ -49,10 +51,11 @@ public class MedicalConsultationAdapter extends ArrayAdapter<MedicalConsultation
 
         for (Appointment auxiliarAppointment : therapist.getAppointments()) {
             if (auxiliarAppointment.getMedicalConsultation() != null && auxiliarAppointment.getMedicalConsultation()
-                           .getMedicalConsultationId() == medicalConsultation.getMedicalConsultationId()) {
+                                                                                           .getMedicalConsultationId() == medicalConsultation.getMedicalConsultationId()) {
                 appointment = auxiliarAppointment;
             }
         }
+
 
         if (medicalConsultation != null && therapist != null && appointment != null) {
             ((TextView) view.findViewById(R.id.medical_consultation_name))
@@ -62,37 +65,11 @@ public class MedicalConsultationAdapter extends ArrayAdapter<MedicalConsultation
                     .setText(medicalConsultation.getEmphasis());
 
             ((TextView) view.findViewById(R.id.medical_consultation_date))
-                    .setText(appointment
-                                     .getStartDate()
-                                     .toString());
+                    .setText(new ApplicationDateFormat().toString(appointment.getStartDate()));
 
             new DownloadImageTask((ImageView) view.findViewById(R.id.medical_consultation_photo))
                     .execute(therapist.getPhotoURL());
         }
         return view;
-    }
-
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
-
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
     }
 }
---
+++ app/src/main/java/com/example/astromedics/helpers/ApplicationDateFormat.java
@@ -0,0 +1,46 @@
+package com.example.astromedics.helpers;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class ApplicationDateFormat {
+
+    public Date Now() {
+        return new Date();
+    }
+
+    public Date createDate(int year, int month, int date) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR,
+                year);
+        cal.set(Calendar.MONTH,
+                month);
+        cal.set(Calendar.DAY_OF_MONTH,
+                date);
+        return cal.getTime();
+    }
+
+    public Date createDate(int year, int month, int date, int hour, int minute) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR,
+                year);
+        cal.set(Calendar.MONTH,
+                month);
+        cal.set(Calendar.DAY_OF_MONTH,
+                date);
+        cal.set(Calendar.HOUR,
+                hour);
+        cal.set(Calendar.MINUTE,
+                minute);
+        return cal.getTime();
+    }
+
+    public String toString(Date date){
+        Locale spanishLocale = new Locale("es",
+                                          "ES");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd 'de' MMMM 'del' yyyy", spanishLocale);
+        return simpleDateFormat.format(date);
+    }
+}
