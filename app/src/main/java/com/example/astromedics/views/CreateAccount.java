6ded92e9720ada7babff86eb41fdd8b426d563ab
dhasane <danihas@live.com>
2020-04-26 19:51:26 -0500

    Imagen de creacion de usuario.

--- app/src/main/java/com/example/astromedics/views/CreateAccount.java
+++ app/src/main/java/com/example/astromedics/views/CreateAccount.java
@@ -6,12 +6,20 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.astromedics.App;
@@ -27,6 +35,9 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class CreateAccount extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
@@ -37,6 +48,11 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
     private Button buttonCorreoPass;
     private CheckBox checkBoxAsMedico;
     private UserService userService;
+
+    private ImageView mImageView;
+
+    static final int IMAGE_PICKER_REQUEST = 7;
+    static final int REQUEST_IMAGE_CAPTURE = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +68,16 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
         this.password_2 =findViewById(R.id.password_register_2);
 
         this.buttonCorreoPass.setOnClickListener(this);
+
+        this.mImageView = findViewById(R.id.imageView_create_user);
+
+        registerForContextMenu( (Button) findViewById(R.id.button_add_image) );
+        ((Button) findViewById(R.id.button_add_image)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openContextMenu((Button) findViewById(R.id.button_add_image));
+            }
+        });
     }
 
     @Override
@@ -90,7 +116,6 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
                     }
                 }
         );
-
     }
 
     private void createPersonBackend(Person p){
@@ -123,4 +148,59 @@ public class CreateAccount extends AppCompatActivity implements View.OnClickList
         }
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu_select_image, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        switch ( item.getItemId() )
+        {
+            case R.id.menu_use_photo_gallery:
+                openGallery();
+                Toast.makeText(getBaseContext(), "galeria", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_take_photo:
+                takePicture();
+                Toast.makeText(getBaseContext(), "foto", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private void openGallery() {
+        Intent pickImage = new Intent(Intent.ACTION_PICK);
+        pickImage.setType("image/*");
+        startActivityForResult(pickImage, IMAGE_PICKER_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+        else if (requestCode == IMAGE_PICKER_REQUEST && resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                mImageView.setImageBitmap(selectedImage);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
