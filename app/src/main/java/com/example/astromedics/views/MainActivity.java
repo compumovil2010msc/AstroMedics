9d7596f5974afbbd59c676975d0915c18b8e9ef9
dhasane <danihas@live.com>
2020-05-23 23:42:01 -0500

    Avances notificacion pedido cita.
    Ya se muestran las notificaciones, pero la lectura/escritura en la
    base de datos de firebase me ha estado dando problemas, entonces no he
    podido probar varias cosas correctamente.
    Cambie el google-services.json y modifique los permisos de acceso del
    realtime database, pero aun sale que no hay permiso.

--- app/src/main/java/com/example/astromedics/views/MainActivity.java
+++ app/src/main/java/com/example/astromedics/views/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.astromedics.views;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +14,7 @@ import com.example.astromedics.R;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private static final String CHANNEL_ID = "AstromedicsNotificationChannel";
     private Button iniciarSesion;
     private Button registro;
 
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         iniciarSesion = findViewById(R.id.button_iniciar_sesion);
         registro = findViewById(R.id.button_registrarse);
+
+        createNotificationChannel();
 
         iniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,5 +49,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         this.finishAffinity();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "channel";
+            String description = "channel description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            //IMPORTANCE_MAX MUESTRA LA NOTIFICACIÓN ANIMADA
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
--- app/src/main/java/com/example/astromedics/views/therapist/HomeTherapist.java
+++ app/src/main/java/com/example/astromedics/views/therapist/HomeTherapist.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -17,10 +19,16 @@ import com.example.astromedics.views.common.fragments.UserSettings;
 import com.example.astromedics.views.therapist.fragments.Reports;
 import com.example.astromedics.views.therapist.fragments.TherapistBookAppointment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeTherapist extends AppCompatActivity {
 
     MenuItem prevMenuItem;
+    private static final String CHANNEL_ID = "AstromedicsNotificationChannel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +37,31 @@ public class HomeTherapist extends AppCompatActivity {
         final BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_user);
         final ViewPager viewPager = findViewById(R.id.viewPagerUser);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
+        // TODO: borrar esto, esta para probar el funcionamiento de notificaciones
+        showNotification("[usuario] ha pedido una cita, entra a la aplicacion para ver los detalles");
+        listenChanges();
+
+        // ejemplo para agregar una segunda base de datos junto a la original
+        //.setApplicationId("1:530266078999:android:481c4ecf3253701e") // Required for Analytics.
+        //.setApiKey("AIzaSyBRxOyIj5dJkKgAVPXRLYFkdZwh2Xxq51k") // Required for Auth.
+        //.setDatabaseUrl("https://project-1765055333176374514.firebaseio.com/") // Required for RTDB.
+
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//                .setApplicationId("autenticacion-ea619") // Required for Analytics.
+//                .setApiKey("AIzaSyDW18AE508FHwZ89xhxtCuQ6JZqa1e66xc")
+//                .setDatabaseUrl("https://autenticacion-ea619.firebaseio.com") // Required for RTDB.
+//                .setStorageBucket("autenticacion-ea619.appspot.com")
+//                .build();
+//        FirebaseApp.initializeApp(this); // ya que no se esta usando para nada mas, hacia falta inicializarla
+        // de forma que pudiera poner la segunda base de datos, para poder realizar las pruebas
+//        FirebaseApp.initializeApp(getApplicationContext(), options, "temporal");
+
+//        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>> se agrega bd secundaria");
+//        startService(
+//                new Intent(this, TherapistNotificationService.class)
+//        );
+
+        bottomNavigationView.setOnNavigationItemSelectedListener( new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
@@ -78,6 +109,68 @@ public class HomeTherapist extends AppCompatActivity {
             }
         });
         setUpViewPager(viewPager);
+    }
+
+    public void listenChanges() {
+        // el usuario en este punto nunca deberia ser null, ya que esto comienza al entrar a HomeTherapist
+        FirebaseUser fu = FirebaseAuth.getInstance().getCurrentUser() ;
+
+        if(fu == null)
+        {
+            System.out.println("el usuario es null");
+            return;
+        }
+
+        // TODO: quitar pruebas
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        // referencia
+//        authFirebaseRef.getRoot().child("sos/emergencyservices"‌​).addValueEventListe‌​ner(new ValueEventListener() { };
+        ref.getRoot().child("citas").setValue("hola");
+        ref.getRoot().child("citas/loquesea").setValue("hola");
+//        ref.setValue("Hello World!");
+        return ;
+
+
+        // Attach a listener to read the data at our posts reference
+        // TODO: encontrar como conseguir permiso para la modificacion/lectura de archivos de la bd
+//        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("citas/" + fu.getUid() );
+//        ref.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+////                Post post = dataSnapshot.getValue(Post.class);
+////                System.out.println(post);
+//                // hacer algo con la info del dataSnapshot
+//                showNotification("[usuario] ha pedido una cita, entra a la aplicacion para ver los detalles");
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                System.out.println("The read failed: " + databaseError.getCode());
+//            }
+//        });
+//        ref.setValue("Hello World!");
+    }
+
+    // para conectarse a la base de datos secundaria
+    DatabaseReference setNewDatabase(String referencePath) {
+        System.out.println(referencePath);
+        FirebaseApp app = FirebaseApp.getInstance("temporal");
+        final FirebaseDatabase db = FirebaseDatabase.getInstance(app);
+        return db.getReference(referencePath);
+    }
+
+    void showNotification(String texto) {
+        Log.i("mensajes", "=====================================oiga, han cambiado los datos");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.planet)
+                .setContentTitle("Astromedics")
+                .setContentText(texto)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(5, builder.build());
     }
 
     private void setUpViewPager(ViewPager viewPager) {
